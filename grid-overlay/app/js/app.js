f467b54f8454afdf65db2584251d6640aa7a3ee8
Mario Knecht <make@logobject.ch>
2018-04-09 16:31:21 +0200

    mobile navi expandable by click

--- grid-overlay/app/js/app.js
+++ grid-overlay/app/js/app.js
@@ -2,11 +2,13 @@ var App = (function(t,s){
 
 var mobileBtn = document.querySelector('.head__mobile');
 var closeBtn = document.querySelector('.mobile__close');
+var categoryBtn = document.querySelector('.mobile__inner');
 
 //init function
 var init = function(){
     mobileBtn.addEventListener('click',openMenu);
     closeBtn.addEventListener('click',closeMenu);
+    categoryBtn.addEventListener('click',openCategory);
 };
 
 
@@ -22,6 +24,14 @@ function closeMenu(e) {
     if(e.target && e.target.matches('button','.mobile__close')){
         var mobileWrap = document.querySelector('.mobile__wrapper');
         mobileWrap.classList.remove('open');
+    }
+}
+
+
+function openCategory(e) {
+    if(e.target && e.target.matches('a','.mobile__title')){
+        console.log(e.target.nextSibling.nextSibling);
+        e.target.nextSibling.nextSibling.classList.toggle('open');
     }
 }
 
