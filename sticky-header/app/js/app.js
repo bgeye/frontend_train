871a692c702bb50ff9a541cbd526148898b3901f
Mario Knecht <make@logobject.ch>
2018-04-10 17:34:46 +0200

    new feature sticky header

---
+++ sticky-header/app/js/app.js
@@ -0,0 +1,23 @@
+var App = (function(t,s){
+
+//var arrayJSON = [];/*'[{"tod":"Salat"},{"tod":"Milch"}]';*/ //only for testing
+
+/*var listKey = 'yourKey'; //key for local storage
+var url = 'http://localhost:3002/todos';//the server
+var localData = t.checkLocalStorage(listKey);*/
+
+//init function
+var init = function(){
+
+    console.log('hello world');
+};
+
+
+
+
+    return{
+        init:init
+    };
+
+})(Tools);
+App.init();
