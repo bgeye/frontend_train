ec18e5d7d64a08d1940f73b7437f8244adfe312c
Mario Knecht <make@logobject.ch>
2018-05-04 16:43:48 +0200

    sticky header grid mario solution

--- sticky-header/app/js/app.js
+++ sticky-header/app/js/app.js
@@ -9,10 +9,59 @@ var localData = t.checkLocalStorage(listKey);*/
 //init function
 var init = function(){
 
-    console.log('hello world');
+    window.addEventListener('scroll',headerScrolling);
 };
 
 
+function headerScrolling(){
+             var pageYOffset = window.pageYOffset;
+             //console.log(pageYOffset);
+             if(pageYOffset > 0){
+                 shrinkHeader();
+                 if(pageYOffset < 2000){
+                     setBigLogo();
+                 }else{
+                     console.log(pageYOffset);
+                     setSmallLogo();
+                 }
+
+
+             }else if(pageYOffset === 0){
+
+                 increaseHeader();
+
+             }
+
+}
+
+
+function shrinkHeader(){
+    var logoBig = document.querySelector('.logo__big');
+    var mainContent = document.querySelector('.main');
+    var header = document.querySelector('.head');
+    header.classList.add('shrink');
+}
+
+function increaseHeader(){
+    var header = document.querySelector('.head');
+    header.classList.remove('shrink');
+}
+
+
+function setSmallLogo() {
+    var logoBig = document.querySelector('.logo__big');
+    var logoSmall = document.querySelector('.logo__small');
+    logoBig.style.display = 'none';
+    logoSmall.style.display = 'inline';
+}
+
+
+function setBigLogo() {
+    var logoBig = document.querySelector('.logo__big');
+    var logoSmall = document.querySelector('.logo__small');
+    logoBig.style.display = 'inline';
+    logoSmall.style.display = 'none';
+}
 
 
     return{
