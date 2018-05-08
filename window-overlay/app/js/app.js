d117919dce1abfae696ae385270936742417a38c
Mario Knecht <make@logobject.ch>
2018-05-08 14:02:35 +0200

    close infobox with outside click

--- window-overlay/app/js/app.js
+++ window-overlay/app/js/app.js
@@ -1,19 +1,32 @@
 var App = (function(t,s){
 
-//var arrayJSON = [];/*'[{"tod":"Salat"},{"tod":"Milch"}]';*/ //only for testing
+/*
+    solution to hide box when outside clicked
+    https://stackoverflow.com/questions/152975/how-do-i-detect-a-click-outside-an-element
+     - eventlistener on overlayer(if not available use window)
+      -> if clicked hide box.
+     - eventlistener on infobox div
+      - if box clicked, check class and stop propagation -> click event goes not to overlayer eventlister where the
+        the box hide function is behind
+      - for close button separate if checker to close also, because it's in the infobox
 
-/*var listKey = 'yourKey'; //key for local storage
-var url = 'http://localhost:3002/todos';//the server
-var localData = t.checkLocalStorage(listKey);*/
+
+*/
 
 var challenge = document.querySelector('.challenge');
+var mainOverlay = document.querySelector('.main__overlay');
+var infoboxWrapper = document.querySelector('.infobox__wrapper');
+var infoboxBtn = 'infobox__btn';
 
 //init function
 var init = function(){
 
-
+    //show infobox
     challenge.addEventListener('click',t.delegate('.infobox__show',showInfobox));
-    challenge.addEventListener('click',t.delegate('.infobox__btn',hideInfobox))
+
+    //hide infobox
+    mainOverlay.addEventListener('click',hideInfobox);
+    infoboxWrapper.addEventListener('click',avoidHideBox);
 
 
     window.addEventListener('scroll',headerScrolling);
@@ -76,6 +89,7 @@ var init = function(){
         var overlay = document.querySelector('.main__overlay');
         overlay.style.display = 'block';
         challenge.addEventListener('click',hideOutside);
+        infoboxBtn = document.querySelector('.infobox_btn');
     }
 
     function hideInfobox(e){
@@ -83,13 +97,40 @@ var init = function(){
         overlay.style.display = 'none';
     }
 
-    function hideOutside(e){
+    function avoidHideBox(e){
         console.log(e.target.className);
-        if(e.target == '' || e.target.className != 'infobox__wrapper'){
+        console.log(infoboxBtn);
+        if(e.target.className == infoboxBtn){
             hideInfobox();
-            challenge.removeEventListener('click',hideOutside);
+        }else{
+
+            e.stopPropagation();
         }
     }
+
+
+//    function hideOutside(e){
+//        //console.log(e.target.className);
+//        console.log(e.target.parentNode.parentNode);
+//        console.log('e.target.className: '+e.target.className);
+//        console.log('e.target.parentNode.parentNode.className: '+e.target.parentNode.parentNode.className);
+//
+//        var infoboxWrapper = document.querySelector('.infobox_wrapper')
+//        if(e.currentTarget.className == infoboxWrapper){
+//            e.stoppropagation();
+//            hideInfobox();
+//            challenge.removeEventListener('click',hideOutside);
+//        }
+//
+//        //console.log(e.currentTarget);
+////        if(e.target.className != 'infobox__wrapper' || e.target.parentNode.parentNode.className != 'infobox__wrapper'){
+////
+////
+////        }else{
+////        hideInfobox();
+////        challenge.removeEventListener('click',hideOutside);
+////        }
+//    }
 
     return{
         init:init
